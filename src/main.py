cd14e9bc1bf425451db5d337d45077e7351bef01
Erik Peteri <peteri.erik@gmail.com>
2023-12-10 19:52:33 +0200

    set pygame window icon and caption

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import pygame
 from renderer import Renderer
 from game_manager import GameManager
+from utils.asset_loader import AssetLoader
 
 pygame.init()
 screen_width, screen_height = 480, 620
 display = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+# set PyGame icon
+icon = AssetLoader().load_image("icon", "favicon.ico")
+pygame.display.set_icon(icon) 
+# set PyGame window caption
+pygame.display.set_caption('Flappy Bird')
 
 game_manager = GameManager(screen_width, screen_height)
 renderer = Renderer(display, screen_width, screen_height, game_manager)
